4027643adac997cb2bd9c5bef514b744727b0570
Andrew Durham <andrewdurham1094@gmail.com>
2021-12-23 16:40:09 -0700

    ran build

---
+++ client/build/precache-manifest.8d4265b2334794616229cd39cc63ed8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d48abb1cab6bf71242189272a0ef58f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a141f96e28ec91d4313",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "2f7118de2a505af828c5",
+    "url": "/static/js/2.268d4a6f.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.268d4a6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a141f96e28ec91d4313",
+    "url": "/static/js/main.2ee0d514.chunk.js"
+  },
+  {
+    "revision": "e90bf6660084d442b2b7",
+    "url": "/static/js/runtime-main.8628acf6.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
